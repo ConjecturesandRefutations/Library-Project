6ad73f83f71e2c32347661c3ccaae7ac6fe1f2da
Alfie <alfiecollins44@gmail.com>
2024-03-10 12:02:20 +0000

    seeding the database

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("./config")(app);
 const capitalize = require("./utils/capitalize");
 const projectName = "library-project";
 
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalize(projectName)} created by Conjectures`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
